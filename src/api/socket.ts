06cc8b0bf9fa32a0371979421cfe72ce680595d9
Quanhh-hehehe <dinhquanganhtran@gmail.com>
2025-12-12 00:16:34 +0700

    Delete src directory

--- src/api/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-export const socket = io("/", {
-    transports: ['polling', 'websocket'], 
-    extraHeaders: {
-        "ngrok-skip-browser-warning": "true"
-    },
-});
