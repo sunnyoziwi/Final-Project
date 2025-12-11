884a924dbe3ba5f523bd3951517e526101d6cc8d
Quanhh-hehehe <dinhquanganhtran@gmail.com>
2025-12-11 13:42:29 +0700

    Update uploadController.js

--- server/uploadController.js
+++ server/uploadController.js
@@ -2,7 +2,6 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 
-// Cấu hình nơi lưu file
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const { userName } = req.body;
@@ -22,20 +21,17 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// Middleware xử lý upload
 exports.uploadMiddleware = upload.single('video');
 
-// Hàm xử lý sau khi upload xong
 exports.handleUpload = (req, res) => {
   if (!req.file) return res.status(400).json({ error: "No file uploaded" });
   const { userName, transcript, index } = req.body;
   const userRootFolder = global.activeSessions[userName];
 
-  // Lưu Transcript (nếu có)
   if (userRootFolder && transcript) {
     const txtFileName = `question_${parseInt(index) + 1}_transcript.txt`;
     
-    // 🔥 SỬA: Trỏ vào folder con 'text'
+    // 
     const txtPath = path.join(userRootFolder, 'text', txtFileName);
     
     fs.writeFileSync(txtPath, transcript, 'utf8');
